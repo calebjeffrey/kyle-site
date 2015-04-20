eeb93a17ceaaa372ec961f2232ec2463a25d586b
caleb jeffrey <calebjeffrey.dev@gmail.com>
2015-04-19 19:19:39 -0700

    leaned out node app

--- app.js
+++ app.js
@@ -4,8 +4,8 @@
 var express = require('express'),
     methodOverride = require('method-override'),
     path = require('path'),
-    bodyParser = require('body-parser'),
-    sendgrid  = require('sendgrid')('calebjeffrey', 'Kairiangel01');
+    bodyParser = require('body-parser');
+    // sendgrid  = require('sendgrid')('calebjeffrey', 'Kairiangel01');
 
 // create express instance
 var app = express();
@@ -29,24 +29,24 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
 
-app.post('/form', function(req, res) {
+// app.post('/form', function(req, res) {
 
-    sendgrid.send({
-        to:       'QandA@ESKluft.com',
-        from:     req.body.email,
-        subject:  'Kluft Form Contact',
-        text:     req.body.message
-        }, function(err, json) {
-            if (err) { return console.error(err); }
-            console.log(json);
-            sendgrid.send({
-                to: req.body.email,
-                from: 'QandA@ESKluft.com',
-                subject: 'Re: your Kluft request',
-                text: 'Hello,\nThank you for reaching out to E.S. Kluft & Company. Your query is important to us.\nWe typically answer emails within 2 business days of receiving them. Our business hours are Monday through Friday 10AM – 6PM PST.\n\nSincerely,\nThe E.S. Kluft & Company team'
-            });
-        });
-});
+//     sendgrid.send({
+//         to:       'QandA@ESKluft.com',
+//         from:     req.body.email,
+//         subject:  'Kluft Form Contact',
+//         text:     req.body.message
+//         }, function(err, json) {
+//             if (err) { return console.error(err); }
+//             console.log(json);
+//             sendgrid.send({
+//                 to: req.body.email,
+//                 from: 'QandA@ESKluft.com',
+//                 subject: 'Re: your Kluft request',
+//                 text: 'Hello,\nThank you for reaching out to E.S. Kluft & Company. Your query is important to us.\nWe typically answer emails within 2 business days of receiving them. Our business hours are Monday through Friday 10AM – 6PM PST.\n\nSincerely,\nThe E.S. Kluft & Company team'
+//             });
+//         });
+// });
 
 app.get('*', function(req, res){
     res.sendFile(__dirname + '/static/index.html');
